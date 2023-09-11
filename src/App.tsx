f90f43c6f70d8c6c250ea8d19f01754835067249
Anthony Golubski-Allen <anthony302367@gmail.com>
2023-09-11 16:36:55 -0400

    Anthony's Addition: BuyShopComponent Page and DisabledShopComponent Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+
 function App() {
   return (
     <div className="grid h-screen w-screen place-items-center">
       <div className="aspect-square h-[40rem] w-[30rem] rounded-lg border-2 border-solid border-black">
         <div>This is a test!</div>
       </div>
+
     </div>
   );
 }
