aebdce2cba0fc0359cbbf72bea562375182fb809
Shane Franklin <shanefranklin148@gmail.com>
2023-09-11 16:26:08 -0400

    Initial play space containing the main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import "./App.css";
-
 function App() {
   return (
-    <>
-      <div>This is a test!</div>
-    </>
+    <div className="grid h-screen w-screen place-items-center">
+      <div className="aspect-square h-[40rem] w-[30rem] rounded-lg border-2 border-solid border-black">
+        <div>This is a test!</div>
+      </div>
+    </div>
   );
 }
 
